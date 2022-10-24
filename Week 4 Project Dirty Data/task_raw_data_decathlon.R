0aad60425adcf1c3f4723a9407d49f510ec4b062
JaneHoggData <112766934+JaneHoggData@users.noreply.github.com>
2022-10-24 15:57:00 +0100

    Dirty Date
    Still struggling with the final question

--- Week 4 Project Dirty Data/task_raw_data_decathlon.R
+++ Week 4 Project Dirty Data/task_raw_data_decathlon.R
@@ -34,18 +34,57 @@ decathlongjump%>%
 
 
 decath100m <-decathlonclean%>%
-  select(decath100m)
+  summarise(mean_100m = mean(`100m`))
  
+decath100m <-decathlonclean%>%
+  group_by(Competition)%>%
+  summarise(mean_100m = mean(`100m`))
+
+
 
 
 Competitionrank <- decathlonclean%>%
-  select("competitorname","Competition", "Points")
+  select("competitorname","Competition","Points")
   
 
+CompetitionrankShot <- decathlonclean%>%
+  select("competitorname","Shot.put", "Competition", "Points")
+  desc
 
+Competitionfour <- decathlonclean%>%
+    group_by("Competitorname","400m","Competition")
+    
+  Competitionfour
+    
+  Competition400a <-decathlonclean%>%
+    group_by(Competition)%>%
+    summarise(mean_400m = mean(`400m`))
   
+    
  
+
   
+  Competitor400 <-decathlonclean%>%
+    select("competitorname","400m","Points")
+  
+  
+  Competitor400 <-decathlonclean%>%
+    select("competitorname","400m","Points")%>%
+    mutate(average_400m_count = if_else(average_400m_count >50))
+           
+  
+  Competitors440 <-decathlonclean%>%
+    select("competitorname","400m")
+    group_by(`400m`)%>%
+    mutate(lessthan_rating) = if_else (less``)
+  
+    
+  
+  
+  books%>%
+    mutate(average_ratings_count = if_else (
+      ratings_count > mean(ratings_count), TRUE, FALSE
+    ), .after = ratings_count)
 
 
 
