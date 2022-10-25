8eaed0a2ce79a4ea7b7c71861c50d083f2e8ccfc
JaneHoggData <112766934+JaneHoggData@users.noreply.github.com>
2022-10-25 16:46:19 +0100

    Dirty Data
    Final

--- Week 4 Project Dirty Data/task_2/raw_data/candy_dirty_data_JaneHogg.R
+++ Week 4 Project Dirty Data/task_2/raw_data/candy_dirty_data_JaneHogg.R
@@ -3,23 +3,25 @@ install.packages("readxl")
 library(readxl)
 boing_boing_candy_2015 <- read_excel("C:/Users/44792/OneDrive/Desktop/Codeclan 2022/dirty_data_codeclanproject_jane_hogg/Week 4 Project Dirty Data/day_5/1_dirty_data_project/dirty_data_project_raw_data/candy_ranking_data/boing-boing-candy-2015.xlsx")
 View(boing_boing_candy_2015)
-boing_boing_candy_2016 <- read_excel("C:/Users/44792/OneDrive/Desktop/Codeclan 2022/dirty_data_codeclanproject_jane_hogg/Week 4 Project Dirty Data/day_5/1_dirty_data_project/dirty_data_project_raw_data/candy_ranking_data/boing-boing-candy-2015.xlsx")
+boing_boing_candy_2016 <- read_excel("C:/Users/44792/OneDrive/Desktop/Codeclan 2022/dirty_data_codeclanproject_jane_hogg/Week 4 Project Dirty Data/day_5/1_dirty_data_project/dirty_data_project_raw_data/candy_ranking_data/boing-boing-candy-2016.xlsx")
 View(boing_boing_candy_2016)
-boing_boing_candy_2017 <- read_excel("C:/Users/44792/OneDrive/Desktop/Codeclan 2022/dirty_data_codeclanproject_jane_hogg/Week 4 Project Dirty Data/day_5/1_dirty_data_project/dirty_data_project_raw_data/candy_ranking_data/boing-boing-candy-2015.xlsx")
+boing_boing_candy_2017 <- read_excel("C:/Users/44792/OneDrive/Desktop/Codeclan 2022/dirty_data_codeclanproject_jane_hogg/Week 4 Project Dirty Data/day_5/1_dirty_data_project/dirty_data_project_raw_data/candy_ranking_data/boing-boing-candy-2017.xlsx")
 View(boing_boing_candy_2017)
 
 library(janitor)
 
 
 candy_2015 <- clean_names(boing_boing_candy_2015)
-
-(candy_2015 <-candy_2015%>%
-rename (going_out =
-           are_you_going_actually_going_trick_or_treating_yourself))
-  
-  
 candy_2016 <-clean_names(boing_boing_candy_2016)
 candy_2017 <-clean_names(boing_boing_candy_2017)
+
+
+XXXX
+
+candy_2015 <-candy_2015%>%
+  rename(trick_or_treating = q1_going_out)
+  
+  
 
 
 
