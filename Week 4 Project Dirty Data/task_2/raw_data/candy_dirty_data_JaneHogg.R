986f8b4c9450347d896b637a78342db50d3d06c8
JaneHoggData <112766934+JaneHoggData@users.noreply.github.com>
2022-10-25 15:55:41 +0100

    Dirty Date Task 4
    Cleaning all the sets together as got in a mess.

--- Week 4 Project Dirty Data/task_2/raw_data/candy_dirty_data_JaneHogg.R
+++ Week 4 Project Dirty Data/task_2/raw_data/candy_dirty_data_JaneHogg.R
@@ -7,18 +7,19 @@ boing_boing_candy_2016 <- read_excel("C:/Users/44792/OneDrive/Desktop/Codeclan 2
 View(boing_boing_candy_2016)
 boing_boing_candy_2017 <- read_excel("C:/Users/44792/OneDrive/Desktop/Codeclan 2022/dirty_data_codeclanproject_jane_hogg/Week 4 Project Dirty Data/day_5/1_dirty_data_project/dirty_data_project_raw_data/candy_ranking_data/boing-boing-candy-2015.xlsx")
 View(boing_boing_candy_2017)
+
 library(janitor)
 
 
 candy_2015 <- clean_names(boing_boing_candy_2015)
-
-
 
 (candy_2015 <-candy_2015%>%
 rename (trick_or_treating =
            are_you_going_actually_going_trick_or_treating_yourself))
   
   
+candy_2016 <-clean_names(boing_boing_candy_2016)
+candy_2017 <-clean_names(boing_boing_candy_2017)
 
 
 
