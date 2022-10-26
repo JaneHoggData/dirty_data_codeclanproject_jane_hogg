a6c1e49447ae4999fd65822c9fc76c8ac2facc2c
JaneHoggData <112766934+JaneHoggData@users.noreply.github.com>
2022-10-26 17:01:41 +0100

    dirty data mega

--- Week 4 Project Dirty Data/task_4_candy/clean_data/clean_data_analysis_janehogg.R
+++ Week 4 Project Dirty Data/task_4_candy/clean_data/clean_data_analysis_janehogg.R
@@ -5,40 +5,24 @@ read.csv(candy_2016)
 read.csv(candy_2017)
 
 
-
-candy_2015age <-candy_2015%>%
-  select("how_old_are_you")%>%
-mean() 
-
-view(candy_2015age)
-
-mean_age <-candy_2015age%>%
-  filter(is_quote == FALSE)%>%
-  group_by(source %>%)
-  summarise(average_age = mean(mean_age))
-
-head(mean_age)
-
-mean_retweets <- codeclan_tweets %>%
-  filter(is_quote == FALSE) %>%
-  group_by(source) %>%
-  summarise(average_retweet = mean(retweet_count))
-
-head(mean_retweets)
-  
-
-  
-  head(mean_candy2015)
-  
-  
-  
-
  
-
-
-
 mega_data <-bind_rows(candy_2015,candy_2016,candy_2017)
 
 
 head(candy_megadata)
+
+
+
+
+
+mega_datecleana <- mega_data%>%
+  select(how_old_are_you,)
+  drop_na()
+
+
+mega_datacleana
+
+mega_datecleana%>%
+  select
+
 
