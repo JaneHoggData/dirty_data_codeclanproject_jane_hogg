c6f6112406657450dab608eeda3837c6635a4c20
JaneHoggData <112766934+JaneHoggData@users.noreply.github.com>
2022-10-27 16:34:38 +0100

    dirty Data candy answer question 2
    yippeee and taking out the infinity

--- Week 4 Project Dirty Data/task_4_candy/clean_data/clean_data_analysis_janehogg.R
+++ Week 4 Project Dirty Data/task_4_candy/clean_data/clean_data_analysis_janehogg.R
@@ -11,18 +11,96 @@ mega_data <-bind_rows(candy_2015,candy_2016,candy_2017)
 
 head(candy_megadata)
 
+#select two columns only 9349 and 2 variable
+mega_datecleana <- mega_data%>%
+  select(how_old_are_you,q1_going_out)
 
-
-
+#saved it to a new vectar meag_datacleana
 
 mega_datecleana <- mega_data%>%
-  select(how_old_are_you,)
+  select(how_old_are_you,q1_going_out)
+  
+
+
+
+
+
+
+  
+
+ mega_cleanb <- mega_datecleana%>%
+  select(how_old_are_you)%>%
   drop_na()
+ 
+ 
+ 
+#this does not work as i need to go back and take out the characters
+ mega_cleanb %>%
+   summarise(mega_cleanb = mean(how_old_are_you))
+ 
+ mega_cleanb%>%
+   
+   #need to change the column type to numeric.
+ 
+ mega_data <- mega_data%>%
+   mutate(how_old_are_you = as.numeric(how_old_are_you))
+ 
+   
+   
+  #this worked
+ 
+ mega_data%>%
+   mutate(how_old_are_you = as.numeric(how_old_are_you))%>%
+    filter(q1_going_out == "No")%>%
+   mutate(average_age = mean(how_old_are_you, na.rm = TRUE))%>%
+   select(average_age)
+ 
+ 
+ #second part of the questions is
+ 
+mega_data%>%
+   mutate(how_old_are_you = as.numeric(how_old_are_you))%>%
+   filter(q1_going_out == "Yes")%>%
+   mutate(average_age = mean(how_old_are_you, na.rm = TRUE))%>%
+   select(average_age)
+ 
+
+ #still getting infinity
+#need to filter out ages <100
+
+   
+mega_datanoinfinity <-mega_data%>%
+  mutate(how_old_are_you = as.numeric(how_old_are_you))%>%
+  filter(q1_going_out == "Yes")%>%
+  filter(how_old_are_you < 100)
 
 
-mega_datacleana
+#this worked
 
-mega_datecleana%>%
-  select
+mega_datanoinfinity <-mega_data%>%
+  
+  
+  treater_averageage <-mega_datanoinfinity%>%
+  mutate(how_old_are_you = as.numeric(how_old_are_you))%>%
+  filter(q1_going_out == "Yes")%>%
+  filter(how_old_are_you < 100)%>%
+  mutate(average_age = mean(how_old_are_you, na.rm = TRUE))%>%
+  select(average_age)
+   
+#this did work - hurrah
+
+   
+   
+
+   
+   
+  
+   
+   
+  
+   
+   
+   
+   
 
 
